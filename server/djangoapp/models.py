80ffd24412a02127a48c2cdba5f011a9acc5d499
alexsiblagan <alexsiblagan@gmail.com>
2024-02-10 14:32:42 -0500

    car model

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car')
+    description = models.CharField(max_length=500)
+
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.first_name + " description: " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -20,8 +28,24 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car')
+    dealer_id = models.IntegerField()
+    Type = models.CharField(null=False, max_length=30, default='car')
+    year = models.DateField(default=now)
+    #models = models.ManyToManyField(CarModel, through='Enrollment')
+    model = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.first_name + " description: " + self.description
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+class CarDealer(models.Model):
+    dealer_id = models.ForeignKey(CarModel, null=True, on_delete=models.CASCADE)
+    dealer_data = models.CharField(max_length=500)
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(models.Model):
+    dealer_id = models.ForeignKey(CarDealer, null=True, on_delete=models.CASCADE)
+    review = models.CharField(max_length=500)
